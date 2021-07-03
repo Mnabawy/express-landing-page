35fefcb48f699491069783de2bfd3cebc28511a2
Mnabawy <m_ninja61@yahoo.com>
2021-07-03 11:20:57 +0200

    implementing delete route

--- controllers/landing.js
+++ controllers/landing.js
@@ -48,7 +48,17 @@ exports.edit_lead = function (req, res, next) {
         id: req.params.id,
       },
     }
-  ).then(result=>{
-    res.redirect("/lead/" + req.params.id )
-  })
+  ).then((result) => {
+    res.redirect("/lead/" + req.params.id);
+  });
+};
+
+exports.delete_lead = function (req, res, next) {
+  return models.Lead.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then(result => {
+    res.redirect('/leads')
+  });
 };
